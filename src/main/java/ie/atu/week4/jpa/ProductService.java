8d5151fcaa8786f4eaa3fb5cbfdcf2be4df5e75e
G00402034 <G00402034@atu.ie>
2024-10-07 18:38:48 +0100

    added method for updating product

--- src/main/java/ie/atu/week4/jpa/ProductService.java
+++ src/main/java/ie/atu/week4/jpa/ProductService.java
@@ -2,6 +2,7 @@ package ie.atu.week4.jpa;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -20,4 +21,16 @@ public class ProductService {
         return productRepo.findAll();
     }
 
+
+    public List<Product> updateProduct(long id, Product updatedProduct) {
+        Product existingProduct = productRepo.findById(id).orElse(null);
+        if (existingProduct != null) {
+            existingProduct.setProductName(updatedProduct.getProductName());
+            existingProduct.setProductDescription(updatedProduct.getProductDescription());
+            existingProduct.setProductPrice(updatedProduct.getProductPrice());
+            productRepo.save(existingProduct);
+        }
+
+        return productRepo.findAll();
+    }
 }
